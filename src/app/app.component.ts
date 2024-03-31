f97f5b206d0cd6a825b2bc60c965a252187510ef
Kyriakos Bagakis <kyrbag@hotmail.gr>
2024-03-31 12:14:17 +0300

    new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   name = 'Kyriakos';
-
-  person ={
-    givenName: 'Kyriakos',
-    surName: 'Bagakis',
-    age: 25,
-    email: 'kyrbag@hotmail.gr',
-  };
 }
