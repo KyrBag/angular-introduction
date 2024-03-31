c5736937fb1f5042bd0b825626677bb925008960
Kyriakos Bagakis <kyrbag@hotmail.gr>
2024-03-31 13:40:14 +0300

    component input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,21 @@ import { PersonTableComponent } from './components/person-table/person-table.com
 })
 export class AppComponent {
   name = 'Kyriakos';
+
+  person0 = 
+   {  
+    givenName: 'Kyriakos',
+    surName: 'Bagakis',
+    age: 25,
+    email: 'kyrbag@hotmail.gr',
+    address: 'Athens, Greece',
+  };
+
+  person1 = {
+    givenName: 'John',
+    surName: 'Doe',
+    age: 0x25,
+    email: 'john@example.com',
+    address: 'New York, USA'
+  };
 }
